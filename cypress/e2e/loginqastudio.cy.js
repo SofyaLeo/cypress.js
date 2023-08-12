a2e45f3c5ab8366cca8de48689dd8fcb0a19aaf8
Sofia <onikorp@yandex.ru>
2023-08-12 17:22:16 +0300

    autotest

--- cypress/e2e/loginqastudio.cy.js
+++ cypress/e2e/loginqastudio.cy.js
@@ -20,4 +20,44 @@ describe('тестирование сайта https://login.qa.studio ' , () => 
         cy.wait(1000);
         cy.get('#messageHeader').should('have.text', 'Успешно отправили пароль на e-mail')
     })
+    it('Проверка не успешной авторизации (пароль)' , () => {
+
+        cy.visit('https://login.qa.studio/')
+
+        cy.get('#mail').type('german@dolnikov.ru')
+        cy.get('#pass').type('iLoeqastudio1')
+        cy.get('#loginButton').click()
+        cy.wait(1000);
+        cy.get('#messageHeader').should('have.text', 'Такого логина или пароля нет')
+})
+it('Проверка не успешной авторизации (логин) ' , () => {
+
+    cy.visit('https://login.qa.studio/')
+
+    cy.get('#mail').type('geran@dolnikov.ru')
+    cy.get('#pass').type('iLoveqastudio1')
+    cy.get('#loginButton').click()
+    cy.wait(1000);
+    cy.get('#messageHeader').should('have.text', 'Такого логина или пароля нет')
+})
+    it('Валидация' , () => {
+
+    cy.visit('https://login.qa.studio/')
+
+    cy.get('#mail').type('gerandolnikov.ru')
+    cy.get('#pass').type('iLoveqastudio1')
+    cy.get('#loginButton').click()
+    cy.wait(1000);
+    cy.get('#messageHeader').should('have.text', 'Нужно исправить проблему валидации')
+})
+    it('Строчные буквы' , () => {
+
+    cy.visit('https://login.qa.studio/')
+
+    cy.get('#mail').type('GerMan@Dolnikov.ru')
+    cy.get('#pass').type('iLoveqastudio1')
+    cy.get('#loginButton').click()
+    cy.wait(1000);
+    cy.get('#messageHeader').should('have.text', 'Такого логина или пароля нет')
+})
 })
